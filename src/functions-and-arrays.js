94b9a824630f1d3673ca0f2be13dd47f95b7ac39
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-02-29 16:48:28 +0100

    Finished

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,32 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1
+  } else if (num1 < num2) {
+    return num2
+  } else {
+    return num1 || num2
+  }
+}
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(long) {
+  let longWord = ""
+
+  if (long.length === 0) {
+    return 0
+  }
+
+  for (const elem of long) {
+    if (elem.length > longWord.length) {
+      longWord = elem
+    }
+  }
+  return longWord
+}
 
 
 
@@ -15,20 +34,50 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arrayNum) {
+  let sum = 0;
 
+  if (arrayNum.length === 0) {
+    return 0;
+  } else if (arrayNum.length === 1) {
+    return arrayNum[0];
+  }
 
+  for (const number of arrayNum) {
+    sum += number;
+  }
+  return sum
 
+}
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arrNum) {
 
+  if (arrNum.length === 0) {
+    return 0;
+  } else if (arrNum.length === 1) {
+    return arrNum[0];
+  }
 
+  let average = sumNumbers(arrNum) / arrNum.length
+  return average
 
+}
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(findWord, elem) {
+  if (findWord.length === 0) {
+    return null;
+  }
+
+  for (let i = 0; i < findWord.length; i++) {
+    if (findWord[i] === elem) {
+      return true
+    }
+  }
+  return false
+}
